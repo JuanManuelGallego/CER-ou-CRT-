658075c2aa3feb25d2a5ecb76d3f88fc1b50f8e0
Juan Gallego <jmgb8000@gmail.cp>
2023-01-17 16:14:01 -0500

    mot de passe

--- APP/client.js
+++ APP/client.js
@@ -10,12 +10,18 @@ const options = {
 
 const client = tls.connect(8000, options, () => {
   console.log('client connected', client.authorized ? 'authorized' : 'unauthorized');
-  client.write('hello\n');
+  //client.write('hello\n');
+
+  const user = { name: "jplemay", password: "123456"};
+
+  client.write(JSON.stringify(user));
 });
+
 client.setEncoding('utf8');
 client.on('data', (data) => {
   console.log(data);
 });
+
 client.on('end', () => {
   console.log('client disconnected');
 });
--- APP/serveur.js
+++ APP/serveur.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const tls = require('tls');
 
+let BD_Users = JSON.parse(fs.readFileSync("DB.json"));
+let globalSocket;
+//console.log(BD_Users);
+
 const options = {
     requestCert: true, // ask for a client cert
     ca: [fs.readFileSync('../autorite/autorite.cer')],
@@ -12,9 +16,29 @@ const server = tls.createServer(options, (socket) => {
   console.log('server connected', socket.authorized ? 'authorized' : 'unauthorized');
   socket.write('welcome!\n');
   socket.setEncoding('utf8');
-  socket.pipe(socket);
+  socket.on('data', onConnData);
+  globalSocket = socket;
 });
 
 server.listen(8000, () => {
   console.log('server bound');
 });
+
+function onConnData(d) {
+    console.log(d.toString());
+
+    let user = JSON.parse(d.toString());
+
+    console.log(user);
+
+    let foundUser = BD_Users.users.find(u => u.name == user.name)
+
+    if(foundUser?.ps == user.password)
+    {
+      globalSocket.write("Connecté")
+    }
+    else
+    {
+      globalSocket.write("Mauvais mot de passe ou usager")
+    }
+} 
